d4eae067e105661244253d76cff3e0539cccccd3
Tarek Selem <t.hamdy91@yahoo.com>
2023-03-21 23:49:30 +0100

    implemented switch theme mode

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,15 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+export const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text>{colorMode}</Text>
+    </HStack>
+  );
+};
