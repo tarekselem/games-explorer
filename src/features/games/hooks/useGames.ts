68cc75b9259524155860c1cf894d72e9e6f91aa9
Tarek Selem <t.hamdy91@yahoo.com>
2023-04-21 22:26:45 +0200

    implemented infinite fetching scrolling

--- src/features/games/hooks/useGames.ts
+++ src/features/games/hooks/useGames.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { useSearchContext } from "@shared/hooks";
-import { ApiClient } from "@api/api-client";
+import { ApiClient, FetchDataResponse } from "@api/api-client";
 import { ISearchFilters } from "@shared/models";
 import { IGame } from "../games-model";
 
@@ -10,28 +11,44 @@ export const useGames = () => {
   const GAMES_CACHE_KEY = ["games", searchFilters];
   const apiClient = new ApiClient<IGame>("/games");
 
-  return useQuery<IGame[], Error>({
+  const infiniteQuery = useInfiniteQuery<FetchDataResponse<IGame>, Error>({
     queryKey: GAMES_CACHE_KEY,
-    queryFn: () =>
-      apiClient.getAll({ params: mainipulateParams(searchFilters) }),
+    queryFn: ({ pageParam = 1 }) =>
+      apiClient.getAllPagination({
+        params: mainipulateParams({ ...searchFilters, page: pageParam }),
+      }),
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.next && allPages.length + 1,
+    staleTime: ms("1h"),
   });
+
+  return {
+    ...infiniteQuery,
+    data: infiniteQuery.data?.pages.flatMap((page) => page.results),
+    totalCount: infiniteQuery.data?.pages.reduce(
+      (total, page) => total + page.results.length,
+      0
+    ),
+  };
 };
 
 const mainipulateParams = ({
+  page,
   pageSize,
   sortOrder,
   searchText,
-  genre,
-  platform,
+  genreId,
+  platformId,
 }: ISearchFilters): { [index: string]: unknown } => {
   const params: { [index: string]: unknown } = {
+    page,
     page_size: pageSize,
   };
 
   if (sortOrder) params["ordering"] = sortOrder;
   if (searchText) params["search"] = searchText;
-  if (genre) params["genres"] = genre.id;
-  if (platform) params["parent_platforms"] = platform.id;
+  if (genreId) params["genres"] = genreId;
+  if (platformId) params["parent_platforms"] = platformId;
 
   return params;
 };
